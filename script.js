32624452ac6821efd157cf3eba05a65ae6ca5149
Shubha Singh <168984679+shubha0503@users.noreply.github.com>
2025-12-21 23:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -206,11 +206,9 @@ function setupVideos() {
     "video2.mp4",
     "video3.mp4",
     "video4.mp4",
-    "video5.mp4",
     "video6.mp4",
     "video7.mp4",
     "video8.mp4",
-    "video9.mp4",
     "video10.mp4",
   ]
 
@@ -480,3 +478,4 @@ window.addEventListener("resize", () => {
     }
   })
 })
+
